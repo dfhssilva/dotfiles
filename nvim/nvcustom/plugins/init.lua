341d4e8e232cc96d04d2f40dc400744e0b3fa8bd
David Silva <dfhssilva@protonmail.com>
2022-03-16 12:20:51 +0000

    Add null-ls and configuration

--- nvim/nvcustom/plugins/init.lua
+++ nvim/nvcustom/plugins/init.lua
@@ -43,6 +43,12 @@ return {
     end
   },
   {
+    -- Null-ls: Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+    "jose-elias-alvarez/null-ls.nvim",
+    after = "nvim-lspconfig",
+    config = function() require("custom.plugins.null-ls").setup() end
+  },
+  {
     -- Stabilize: a plugin to stabilize window open/close events
     "luukvbaal/stabilize.nvim",
     config = function() require("stabilize").setup() end
