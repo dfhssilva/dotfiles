a56c4beed4c805c89d9317ebc96475f368c01496
David Silva <dfhssilva@protonmail.com>
2022-03-27 11:23:35 +0100

    Remove nvim autosave

--- nvim/nvcustom/plugins/init.lua
+++ nvim/nvcustom/plugins/init.lua
@@ -1,64 +1,43 @@
 -- Plugins file
 
 return {
-  {
-    -- AutoSave: saving your work before the world collapses
-    "Pocco81/AutoSave.nvim",
-    config = function()
-      local autosave = require "autosave"
-      autosave.setup {
-        enabled = true,
-        execution_message = " at: " .. vim.fn.strftime("%H:%M:%S"),
-        events = { "InsertLeave", "TextChanged" },
-        conditions = {
-          exists = true,
-          filename_is_not = {},
-          filetype_is_not = {},
-          modifiable = true,
-        },
-        clean_command_line_interval = 2500,
-        on_off_commands = false,
-        write_all_buffers = false,
-        debounce_delay = 250
-      }
-    end,
-    setup = function()
-      require("core.utils").packer_lazy_load "AutoSave.nvim"
-    end
-  },
-  {
-    -- LSP-installer: allows you to seamlessly manage LSP servers locally
-    "williamboman/nvim-lsp-installer",
-    opt = true,
-    config = function()
-      local lsp_installer = require "nvim-lsp-installer"
-      lsp_installer.on_server_ready(function(server)
-        local opts = {}
-        server:setup(opts)
-        vim.cmd [[ do User LspAttachBuffers ]]
-      end)
-    end,
-    setup = function()
-      require("core.utils").packer_lazy_load "nvim-lsp-installer"
-    end
-  },
-  {
-    -- Null-ls: Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-    "jose-elias-alvarez/null-ls.nvim",
-    after = "nvim-lspconfig",
-    config = function() require("custom.plugins.null-ls").setup() end
-  },
-  {
-    -- Stabilize: a plugin to stabilize window open/close events
-    "luukvbaal/stabilize.nvim",
-    config = function() require("stabilize").setup() end
-  },
-  {
-    -- capslock.vim: Software caps lock
-    "tpope/vim-capslock"
-  },
-  {
-    -- tmux_navigator.vim: Seamless navigation between tmux panes and vim splits
-    "christoomey/vim-tmux-navigator"
-  }
+	{
+		-- LSP-installer: allows you to seamlessly manage LSP servers locally
+		"williamboman/nvim-lsp-installer",
+		opt = true,
+		config = function()
+			local lsp_installer = require("nvim-lsp-installer")
+			lsp_installer.on_server_ready(function(server)
+				local opts = {}
+				server:setup(opts)
+				vim.cmd([[ do User LspAttachBuffers ]])
+			end)
+		end,
+		setup = function()
+			require("core.utils").packer_lazy_load("nvim-lsp-installer")
+		end,
+	},
+	{
+		-- Null-ls: Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+		"jose-elias-alvarez/null-ls.nvim",
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls").setup()
+		end,
+	},
+	{
+		-- Stabilize: a plugin to stabilize window open/close events
+		"luukvbaal/stabilize.nvim",
+		config = function()
+			require("stabilize").setup()
+		end,
+	},
+	{
+		-- capslock.vim: Software caps lock
+		"tpope/vim-capslock",
+	},
+	{
+		-- tmux_navigator.vim: Seamless navigation between tmux panes and vim splits
+		"christoomey/vim-tmux-navigator",
+	},
 }
