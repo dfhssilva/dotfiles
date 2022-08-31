9964114f345e57ad7fca309dba8ff3152e6ec944
David Silva <dfhssilva@protonmail.com>
2022-08-31 03:02:44 +0100

    Add configs for dynamic BSPWM monitor setup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,3 +8,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
 polybar top -c ~/.config/polybar/config.ini &
+
+# Launch polybar on external monitor if connected
+external_monitor=HDMI2
+if [[ $(xrandr -q | grep "$external_monitor connected") ]]; then
+    polybar top_external -c ~/.config/polybar/config.ini &
+fi
